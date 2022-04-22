7a0f3ba701db8a51b16d68a731027eddf2197642
DimitriCodeur <dimitriromaric864@gmail.com>
2022-04-22 14:25:13 +0200

    Top Banner Component

--- site/src/App.js
+++ site/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import TopBanner from "./assets/components/TopBanner/TopBanner";
+import './assets/css/custom.css';
+import './assets/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
     
+        <TopBanner />
 
     </div>
   );
